f8b4a96607f0e069a1a00421c429d06081b1ed40
JonathanAlcantara <jonathan.alcantara@poli.ufrj.br>
2017-11-15 22:50:06 -0200

    Página de Configuração Desenvolvida
    A página nasce quando selecionada pelo sidebar, mostra a lista com os laboratórios e as salas internas. Se o usuário clicar na sala, abre um alert com os dados do MAC da sala.

--- issuesauthenticatorcode/src/pages/config/config.ts
+++ issuesauthenticatorcode/src/pages/config/config.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";  
+import { NavController, AlertController } from 'ionic-angular'; 
 
 @Component({
-  selector: 'page-config',
-  templateUrl: 'config.html'
+    selector: 'page-config',
+    templateUrl: 'config.html'
 })
+export class ConfigPage {  
 
-export class ConfigPage {
+    constructor(private navCtrl: NavController, public alertCtrl: AlertController) {
+    }
 
-  constructor(public navCtrl: NavController) {
+    getRouters() {
+    }
 
-  }
-
+    doAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Configuração da Sala',
+      message: 'MAC: 00-80-F0-32-0A-7A',
+      buttons: ['Ok']
+    });
+    alert.present()
+}
 }
