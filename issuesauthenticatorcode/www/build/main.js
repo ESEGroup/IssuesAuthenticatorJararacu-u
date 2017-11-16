7bbf459756f36c51f260670ad0c54884cb7b7bec
Unknown <raphaelfilene@hotmail.com>
2017-11-15 23:19:00 -0200

    Implementei a página home(login)
    Implementei a página que será usada para realizar o login. Falta agora torná-la 'intro page' sem que isso afete as sidemenus das outras páginas.

--- issuesauthenticatorcode/www/build/main.js
+++ issuesauthenticatorcode/www/build/main.js
@@ -60,7 +60,7 @@ var HomePage = (function () {
 }());
 HomePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-home',template:/*ion-inline-start:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    <ion-title>IssuesAuthenticator</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="bg-style">\n  <img src="wifi.png" height="200" width="200" class="img-center">\n</ion-content>'/*ion-inline-end:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/home/home.html"*/
+        selector: 'page-home',template:/*ion-inline-start:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\home\home.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <button ion-button menuToggle>\n\n      <ion-icon name="menu"></ion-icon>\n\n    </button>\n\n    <ion-title>IssuesAuthenticator</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content>\n\n  <form action="#">\n\n		<p><input type="text" placeholder="Enter Username" id="username" required></p>\n\n		<p><input type="password" placeholder="Enter Password" id="password" required></p>\n\n		<button type="submit">Login</button>\n\n		<p><a href="#">Forgot your password?</a></p>\n\n		<a id="forgot" (click)="irParaPaginaPrincipal()">Pular</a>\n\n	</form>\n\n</ion-content>'/*ion-inline-end:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\home\home.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]])
 ], HomePage);
@@ -95,7 +95,7 @@ var ProfilePage = (function () {
 }());
 ProfilePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-profile',template:/*ion-inline-start:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/profile/profile.html"*/'<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    <ion-title>Preferências</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="bg-style">\n  <img src="wifi.png" height="200" width="200" class="img-center"> \n</ion-content>\n'/*ion-inline-end:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/profile/profile.html"*/
+        selector: 'page-profile',template:/*ion-inline-start:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\profile\profile.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <button ion-button menuToggle>\n\n      <ion-icon name="menu"></ion-icon>\n\n    </button>\n\n    <ion-title>Preferências</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding class="bg-style">\n\n  <img src="wifi.png" height="200" width="200" class="img-center"> \n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\profile\profile.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]])
 ], ProfilePage);
@@ -130,7 +130,7 @@ var ConfigPage = (function () {
 }());
 ConfigPage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-config',template:/*ion-inline-start:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/config/config.html"*/'<ion-header>\n  <ion-navbar>\n    <button ion-button menuToggle>\n      <ion-icon name="menu"></ion-icon>\n    </button>\n    <ion-title>Configurações</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="bg-style">\n  <img src="wifi.png" height="200" width="200" class="img-center"> \n</ion-content>'/*ion-inline-end:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/pages/config/config.html"*/
+        selector: 'page-config',template:/*ion-inline-start:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\config\config.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <button ion-button menuToggle>\n\n      <ion-icon name="menu"></ion-icon>\n\n    </button>\n\n    <ion-title>Configurações</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding class="bg-style">\n\n  <img src="wifi.png" height="200" width="200" class="img-center"> \n\n</ion-content>'/*ion-inline-end:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\pages\config\config.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]])
 ], ConfigPage);
@@ -292,15 +292,14 @@ var MyApp = (function () {
 }());
 __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_14" /* ViewChild */])(__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* Nav */]),
-    __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* Nav */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* Nav */]) === "function" && _a || Object)
+    __metadata("design:type", __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* Nav */])
 ], MyApp.prototype, "nav", void 0);
 MyApp = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/app/app.html"*/'<ion-menu [content]="content">\n  <ion-header>\n    <ion-toolbar>\n      <ion-title>Menu</ion-title>\n    </ion-toolbar>\n  </ion-header>\n\n  <ion-content>\n    <ion-list>\n      <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">\n        {{p.title}}\n      </button>\n    </ion-list>\n  </ion-content>\n\n</ion-menu>\n\n<!-- Disable swipe-to-go-back because it\'s poor UX to combine STGB with side menus -->\n<ion-nav [root]="rootPage" #content swipeBackEnabled="false"></ion-nav>'/*ion-inline-end:"/home/joao/IssuesAuthenticatorJararacucu/issuesauthenticatorcode/src/app/app.html"*/
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\app\app.html"*/'<ion-menu [content]="content">\n\n  <ion-header>\n\n    <ion-toolbar>\n\n      <ion-title>Menu</ion-title>\n\n    </ion-toolbar>\n\n  </ion-header>\n\n\n\n  <ion-content>\n\n    <ion-list>\n\n      <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">\n\n        {{p.title}}\n\n      </button>\n\n    </ion-list>\n\n  </ion-content>\n\n\n\n</ion-menu>\n\n\n\n<!-- Disable swipe-to-go-back because it\'s poor UX to combine STGB with side menus -->\n\n<ion-nav [root]="rootPage" #content swipeBackEnabled="false"></ion-nav>'/*ion-inline-end:"C:\Users\Filene\Desktop\dev_Raphael\IssuesAuthenticatorJararacucu\issuesauthenticatorcode\src\app\app.html"*/
     }),
-    __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* Platform */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* Platform */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]) === "function" && _d || Object])
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]])
 ], MyApp);
 
-var _a, _b, _c, _d;
 //# sourceMappingURL=app.component.js.map
 
 /***/ }),
