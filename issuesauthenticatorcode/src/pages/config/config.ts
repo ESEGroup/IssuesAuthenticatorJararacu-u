bdea430155d19f79f5fc0bf09315bf577d6b2944
JonathanAlcantara <jonathan.alcantara@poli.ufrj.br>
2017-12-03 22:11:00 -0200

    Updating the Android App metadata
    Giving a name and an icon to the app

--- issuesauthenticatorcode/src/pages/config/config.ts
+++ issuesauthenticatorcode/src/pages/config/config.ts
@@ -7,18 +7,18 @@ import { NavController, AlertController } from 'ionic-angular';
 })
 export class ConfigPage {  
 
-    constructor(private navCtrl: NavController, public alertCtrl: AlertController) {
+    constructor(/*private navCtrl: NavController, */public alertCtrl: AlertController) {
     }
 
     getRouters() {
     }
 
     doAlert() {
-    let alert = this.alertCtrl.create({
-      title: 'Configuração da Sala',
-      message: 'MAC: 00-80-F0-32-0A-7A',
-      buttons: ['Ok']
-    });
-    alert.present()
-}
+      let alert = this.alertCtrl.create({
+        title: 'Configuração da Sala',
+        message: 'MAC: 00-80-F0-32-0A-7A',
+        buttons: ['Ok']
+      });
+      alert.present()
+    }
 }
